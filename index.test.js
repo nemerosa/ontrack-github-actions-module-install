c2884cd8157e65de4b5e5cd848115f508dbc42e0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-11-08 13:48:14 +0100

    release: testing the configuration

--- index.test.js
+++ index.test.js
@@ -1,12 +1,14 @@
 const cli = require('./index')
 const {execSync} = require('child_process')
 
+const testVersion = '5.0.0-alpha+006'
+
 test('downloading the CLI using a fixed version', async () => {
-    const {dir, cliExecutable} = await cli.install({logging: true, version: '5.0.0-alpha+006'})
+    const {dir, cliExecutable} = await cli.install({logging: true, version: testVersion})
     const exe = `${dir}/${cliExecutable}`
 
     const output = execSync(`${exe} version --cli`, {encoding: 'utf-8'}).trim()
-    expect(output).toBe('5.0.0-alpha+006')
+    expect(output).toBe(testVersion)
 })
 
 test('downloading the CLI using the latest version', async () => {
@@ -19,4 +21,23 @@ test('downloading the CLI using the latest version', async () => {
 
     const output = execSync(`${exe} version --cli`, {encoding: 'utf-8'}).trim()
     expect(output).not.toBe('')
+})
+
+test('creation of the configuration', async () => {
+    const yontrackUrl = process.env.YONTRACK_URL
+    const yontrackToken = process.env.YONTRACK_TOKEN
+    if (!yontrackUrl || !yontrackToken) {
+        throw new Error('YONTRACK_URL and YONTRACK_TOKEN environment variables must be set')
+    }
+
+    const {dir, cliExecutable} = await cli.install({
+        logging: true,
+        version: testVersion,
+        yontrackUrl,
+        yontrackToken,
+    })
+    const exe = `${dir}/${cliExecutable}`
+
+    const output = execSync(`${exe} version`, {encoding: 'utf-8'}).trim()
+    console.log(output)
 })
